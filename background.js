aa8f2c38c1e239472b153d06ba4a48ec96287179
ecsd-gituser <ecsd.github@eagleschools.net>
2024-10-17 12:14:51 -0600

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.alarms.create("sessionTimeout", { delayInMinutes: 1 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "sessionTimeout") {
-    chrome.browserAction.setBadgeText({ text: "END" });
-    console.log("Session ended after 1 minute");
+    console.log("Session timeout triggered after 1 minute");
+    chrome.runtime.restart();
   }
 });
